b66850e1b6bd0dde51b5bb8dde73447064f2432c
danimilles <dansanbal@alum.us.es>
2021-05-15 21:44:17 +0200

    Populacion de datos de prueba

--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as log_out, login as log_in, authenticate
 import pymongo
+import json
+
 
 from Mobooks.settings import DB_HOST
 
@@ -48,5 +50,29 @@ def connect_to_db():
 
 
 def populate_database(request):
-    mobooks_db = connect_to_db
+    mobooks_db = connect_to_db()
+    books_collection = mobooks_db['books']
+    books_collection.drop()
+    with open('static/pop_data/books.json') as file:
+        data = json.load(file)
+        books_collection.insert_many(data)
+
+    authors_collection = mobooks_db['authors']
+    authors_collection.drop()
+    with open('static/pop_data/authors.json') as file:
+        data = json.load(file)
+        authors_collection.insert_many(data)
+
+    genres_collection = mobooks_db['genres']
+    genres_collection.drop()
+    with open('static/pop_data/genres.json') as file:
+        data = json.load(file)
+        genres_collection.insert_many(data)
+
+    publishers_collection = mobooks_db['publishers']
+    publishers_collection.drop()
+    with open('static/pop_data/publishers.json') as file:
+        data = json.load(file)
+        publishers_collection.insert_many(data)
+        
     return render(request, 'index.html', {})
